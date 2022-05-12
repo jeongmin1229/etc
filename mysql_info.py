fa466b8dca0e0ce82a041d772ad9b4728663d3f5
jeongmin <86039081+jeongmin1229@users.noreply.github.com>
2022-05-12 14:21:33 +0900

    Create mysql_info.py

---
+++ mysql_info.py
@@ -0,0 +1,9 @@
+# 설치를 완료했다면, 먼저 아래의 명령어를 입력해주자.
+
+# brew install mysql
+# 그다음 아래의 명령어로 OpenSSL을 설치해 준다.
+
+# brew install openssl
+# 설치가 완료된 후, 아래의 명령어로 mysqlclient를 설치해 준다.
+
+# LDFLAGS=-L/usr/local/opt/openssl/lib pip install mysqlclient
